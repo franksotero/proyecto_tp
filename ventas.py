dddf28528e767c3e9317feb1ac3064992314660e
Frank <frsotero@uade.edu.ar>
2026-09-02 14:00:49 -0300

    Correccion registrar ventas

--- ventas.py
+++ ventas.py
@@ -1,5 +1,5 @@
 from productos import productos
-from clientes import clientes
+from clientes import clientes, listar_clientes
 
 ventas = []
 
@@ -7,12 +7,14 @@ ventas = []
 def menu_ventas():
     opcion = ""
     while opcion != "5":
-        print("----MENU DE VENTAS-----")
-        print("1. REGISTRAR VENTAS ")
-        print("2.CONSULTAR VENTAS")
-        print("3.REPORTE DE VENTAS")
-        print("4. CANCELAR VENTA")
-        print("5. VOLVER A MENU PRINCIPAL")
+        print("\n=================================")
+        print("        GESTIÓN DE VENTAS         ")
+        print("=================================")
+        print("1. Registrar ventas ")
+        print("2. Consultar ventas")
+        print("3. Reporte de ventas")
+        print("4. Cancelar venta")
+        print("5. Volver al menu principal")
         opcion = input("Elija una opcion:")
         if opcion == "1":
             registrar_venta()
@@ -23,39 +25,35 @@ def menu_ventas():
         elif opcion == "4":
             cancelar_venta()
         elif opcion == "5":
-            print("REGRESANDO AL MENU PRINCIPAL")
+            print("Regresando al menu principal...L")
         else:
             print("opcion invalida")
 
 
 def registrar_venta():
     print(" Registrar venta")
-    cliente_id = input("Ingrese ID del cliente: ")
+    listar_clientes()
 
+    cliente_id = int(input("Ingrese ID del cliente: "))
     cliente = None
     for c in clientes:
         if c[0] == cliente_id:  # ID está en posición 0
             cliente = c
-
     if cliente == None:
         print("Cliente no encontrado.")
         return
-
     prod_id = int(input("Ingrese ID del producto: "))
     producto = None
     for p in productos:
         if p[0] == prod_id:
             producto = p
-
     if producto == None:
         print("Producto no encontrado.")
         return
-
     cantidad = int(input("Ingrese cantidad: "))
     if cantidad > producto[4]:
         print("Stock insuficiente.")
         return
-
     producto[4] = producto[4] - cantidad  # actualizar stock
     precio = producto[3]
     descuento = producto[5]
