d1393fd734c2cc8d609800c5c7b4c99f875cc47f
Dan <169019939+CarlosDanielOK@users.noreply.github.com>
2026-09-14 04:29:44 -0300

    modularizacion de listar ventas

--- ventas.py
+++ ventas.py
@@ -68,12 +68,38 @@ def buscar_producto_por_id(producto_id):
 
 def buscar_venta_por_id(venta_id):
     """
-        Busca una venta utilizando su ID.
-        """
+    Busca una venta utilizando su ID.
+    """
     for venta in ventas:
         if venta["id"] == venta_id:
             return venta
     return None
+
+
+def mostrar_ventas():
+    print("\n\033[1;33;44m---- LISTA DE VENTAS ----\033[0m")
+    print(
+        f"{'ID VENTA':<8} | {'ID CLIENTE':<10} | {'NOMBRE Y APELLIDO':<17} | {'ITEMS':<5} | {'UNIDADES':<8} | {'TOTAL':<10}"
+    )
+    print("-" * 70)
+    for venta in ventas:
+        print(
+            f"{venta['id']:<8} | {venta['cliente_id']:<10} | {venta['cliente_nombre']:<17} | {len(venta['items']):<5} | {sum(item['cantidad'] for item in venta['items']):<8} | ${venta['total']:<10}"
+        )
+
+
+def mostrar_detalles_venta(venta):
+    print(f"\nDetalles de la venta ID {venta['id']}:")
+    print(
+        f"{'ID PRODUCTO':<12} | {'NOMBRE Y APELLIDO':<22} | {'CATEGORÍA':<12} | {'CANTIDAD':<8} | {'PRECIO UNITARIO':<15} | {'DESCUENTO':<10} | {'SUBTOTAL':<10} | {'PRECIO FINAL':<12}"
+    )
+    print("-" * 120)
+    for item in venta["items"]:
+        print(
+            f"{item['producto_id']:<12} | {item['nombre']:<22} | {item['categoria']:<12} | {item['cantidad']:<8} | ${item['precio_unitario']:<14.2f} | {item['descuento'] * 100:<9.1f}% | ${item['subtotal']:<9} | ${item['precio_final']:<11}"
+        )
+    print("-" * 120)
+    print(f"Total de la venta: ${venta['total']}")
 
 
 # CREAR
@@ -155,20 +181,14 @@ def registrar_ventas(rol):
     print("\033[32m\nVenta registrada correctamente.\033[0m")
     print(f"Total de la venta: ${diccionario_venta['total']}")
 
+
 # LEER
 def listar_ventas():
     """
     Muestra el historial resumido de ventas y permite consultar los detalles de una venta especifica.
     """
-    print("\n\033[1;33;44m---- LISTA DE VENTAS ----\033[0m")
-    print(
-        f"{'ID VENTA':<8} | {'ID CLIENTE':<10} | {'NOMBRE Y APELLIDO':<17} | {'ITEMS':<5} | {'UNIDADES':<8} | {'TOTAL':<10}"
-    )
-    print("-" * 70)
-    for venta in ventas:
-        print(
-            f"{venta['id']:<8} | {venta['cliente_id']:<10} | {venta['cliente_nombre']:<17} | {len(venta['items']):<5} | {sum(item['cantidad'] for item in venta['items']):<8} | ${venta['total']:<10}"
-        )
+
+    mostrar_ventas()
 
     venta_id = int(
         input("\nIngrese el ID de la venta para ver detalles o presione 0 para salir: ")
@@ -176,21 +196,12 @@ def listar_ventas():
     if venta_id == 0:
         return
 
-    for venta in ventas:
-        if venta["id"] == venta_id:
-            print(f"\nDetalles de la venta ID {venta_id}:")
-            print(
-                f"{'ID PRODUCTO':<12} | {'NOMBRE Y APELLIDO':<22} | {'CATEGORÍA':<12} | {'CANTIDAD':<8} | {'PRECIO UNITARIO':<15} | {'DESCUENTO':<10} | {'SUBTOTAL':<10} | {'PRECIO FINAL':<12}"
-            )
-            print("-" * 120)
-            for item in venta["items"]:
-                print(
-                    f"{item['producto_id']:<12} | {item['nombre']:<22} | {item['categoria']:<12} | {item['cantidad']:<8} | ${item['precio_unitario']:<14.2f} | {item['descuento'] * 100:<9.1f}% | ${item['subtotal']:<9} | ${item['precio_final']:<11}"
-                )
-            print("-" * 120)
-            print(f"Total de la venta: ${venta['total']}")
-            return
-    print("\033[31mID de venta no encontrado.\033[0m")
+    venta = buscar_venta_por_id(venta_id)
+    if venta is None:
+        print("\033[31mID de venta no encontrado.\033[0m")
+        return
+
+    mostrar_detalles_venta(venta)
 
 
 # ACTUALIZAR
